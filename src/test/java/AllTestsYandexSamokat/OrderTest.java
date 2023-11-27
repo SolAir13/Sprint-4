43ac168620e4a20a7f802750703f55430bcc3b67
Alexey Solomatin <asolomatin94@yandex.ru>
2023-11-27 21:40:11 +0300

    Fixed lounch browser and set one buttom on two click (top and bottom)

--- src/test/java/AllTestsYandexSamokat/OrderTest.java
+++ src/test/java/AllTestsYandexSamokat/OrderTest.java
@@ -1,30 +1,57 @@
 package AllTestsYandexSamokat;
 
 import ButtonAndPath.ButtonOnPageObject;
-import Configure.WebDriverFactory;
+//import Configure.WebDriverFactory;
 import ElementsOnFormOrder.AboutRentElementsOnOrderPage;
 import ElementsOnFormOrder.OrderPageElements;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
-
+@RunWith(Parameterized.class)
 public class OrderTest {
     private WebDriver driver;
+
+    @Parameterized.Parameter
+    public String buttonLocation;
+
+    @Parameterized.Parameters
+    public static Object[][] testData() {
+        return new Object[][]{
+                {"top"}, {"bottom"}
+        };
+    }
     String website = "https://qa-scooter.praktikum-services.ru/";
+
     @Before
-    public void setup() throws Exception {
-        String browserName = WebDriverFactory.getBrowserName();
-        driver = WebDriverFactory.getWebDriverForBrowser(browserName);
-        WebDriverFactory.navigateToTheUrl(website);
+    public void setup() {
+        switch (System.getProperty("browser")) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+                this.driver = new ChromeDriver();// инициализация firefox
+                driver.get(website);
+                break;
+            case "firefox":
+            default:
+                WebDriverManager.firefoxdriver().setup();
+                System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver");
+                this.driver = new FirefoxDriver();
+                driver.get(website);
+        }
     }
 
     @Test
-    // в этом тесте бага при нажатии на кнопку "Да"
+    // в этом тесте бага при нажатии на кнопку "Да" для браузера chrome. Для браузера firefox тесты проходят
     public void testChromeWhenButtonYesDontClick() {
         ButtonOnPageObject buttonOnPageObject = new ButtonOnPageObject(driver);
-        buttonOnPageObject.clickOrderButtonOnUp();
+        buttonOnPageObject.selectButtonOrderOnHomePage(buttonLocation);
         buttonOnPageObject.waitForLoadHeaderOrderPage();
         OrderPageElements orderPageElements = new OrderPageElements(driver);
         orderPageElements.inputFirstNameClient("Алекс");
@@ -48,36 +75,9 @@ public class OrderTest {
         aboutRentElementsOnOrderPage.clickButtonOrderStatus();
     }
 
-    @Test
-    public void testMozilaWhenButtonYesClick() {
-        ButtonOnPageObject buttonOnPageObject = new ButtonOnPageObject(driver);
-        buttonOnPageObject.clickOrderButtonOnUp();
-        buttonOnPageObject.waitForLoadHeaderOrderPage();
-        OrderPageElements orderPageElements = new OrderPageElements(driver);
-        orderPageElements.inputFirstNameClient("Антон");
-        orderPageElements.inputLastNameClient("Тестовский");
-        orderPageElements.inputAddressClient("119021, г. Москва, ул. Льва Толстого, д. 16");
-        orderPageElements.inputMetroStationClient("Киевская");
-        orderPageElements.inputPhoneClient("+79229002277");
-        orderPageElements.clickFurtherButton();
-        orderPageElements.waitForLoadHeaderAboutRentPage();
-        AboutRentElementsOnOrderPage aboutRentElementsOnOrderPage = new AboutRentElementsOnOrderPage(driver);
-        aboutRentElementsOnOrderPage.inputWhereToBringFemale("23.11.2023");
-        aboutRentElementsOnOrderPage.clickRentalPeriod();
-        aboutRentElementsOnOrderPage.choiceRentalPeriod();
-        aboutRentElementsOnOrderPage.clickColorFamele();
-        aboutRentElementsOnOrderPage.clickChoiceColorFamele();
-        aboutRentElementsOnOrderPage.inputCommentClient("Привезите самокат получше, который едет быстрее всех");
-        aboutRentElementsOnOrderPage.clickOrderButton();
-        aboutRentElementsOnOrderPage.clickClientButtonYes();
-        aboutRentElementsOnOrderPage.waitForLoadPageOrder();
-        aboutRentElementsOnOrderPage.waitOrderSuccsessScreen();
-        aboutRentElementsOnOrderPage.clickButtonOrderStatus();
-    }
-
     @After
     public void closeBrow() {
-        WebDriverFactory.quitDriver();
+        driver.quit();
     }
 
 }
--- src/test/java/ButtonAndPath/ButtonOnPageObject.java
+++ src/test/java/ButtonAndPath/ButtonOnPageObject.java
@@ -7,37 +7,46 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
-
 public class ButtonOnPageObject {
     private static WebDriver driver;
     private By orderButtonUpOnHomePage = By.xpath(".//div[@class='Header_Nav__AGCXC']/button[@class='Button_Button__ra12g']"); //локатор кнопки "Заказать" вверху страницы
+
+    private By orderButtonDownOnHomePage = By.xpath(".//div[@class='Home_FinishButton__1_cWm']/button[text()='Заказать']");//локатор кнопки Заказать в нижней части главной страницы
+
     private By titleElementPageOrder = By.xpath(".//div[text()='Для кого самокат']");
 
     private static final By components = By.xpath(".//div[@data-accordion-component= 'AccordionItem']");
-
 
 
     public ButtonOnPageObject(WebDriver driver) {
         this.driver = driver;
     }
 
-    public void clickOrderButtonOnUp() {
-        driver.findElement(orderButtonUpOnHomePage).click();// Нажатие на кнопку "Заказать" вверху страницы
+    public void selectButtonOrderOnHomePage(String buttonLocation) {
+        if (buttonLocation.equals("top")) {
+            driver.findElement(orderButtonUpOnHomePage).click();
+        } else if (buttonLocation.equals("bottom")) {
+            WebElement element = driver.findElement(orderButtonDownOnHomePage);
+            scrollToElementOnPage(element);
+            element.click();
+        } else {
+            System.out.println("Такой кнопки не сущетсвует!");
+        }
     }
 
-    public void waitForLoadHeaderOrderPage(){
+    public void waitForLoadHeaderOrderPage() {
         new WebDriverWait(driver, 3)
                 .until(ExpectedConditions.visibilityOfElementLocated(titleElementPageOrder)); //ожидание загрузки заголовка Для кого самокат
     }
-    public static void scrollPage(WebElement webElement){
+
+    public static void scrollToElementOnPage(WebElement webElement) {
         // Прокрутить страницу до вопросов
-        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", webElement);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", webElement);
     }
 
     public static ParametrsOnMainPage getDataQandA(int index) {
         WebElement webElement = driver.findElements(components).get(index);
-        scrollPage(webElement);
+        scrollToElementOnPage(webElement);
         webElement.click();
         return new ParametrsOnMainPage(webElement, driver); //получение компонента из списка
     }
